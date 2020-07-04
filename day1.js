c2e42384fc87558c67699f721ea8f0a3dad390a3
--replace-all <shahidbangashsi94@gmail.com>
2020-07-05 00:30:46 +0500

    forst commit

---
+++ day1.js
@@ -0,0 +1,11 @@
+// Here i will use array some methods 
+
+
+var my_array = [2 ,"HI",3,3]
+
+for ( index = 0; index < my_array.length; index++) {
+   console.log(my_array[index]);
+    
+}
+
+console.log(typeof(my_array));
